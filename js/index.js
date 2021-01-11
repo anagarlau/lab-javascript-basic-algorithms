7124cbd6762e50952993bcd1c40c22516b03b7b4
Ana-Maria Garlau <garlauanamaria@gmail.com>
2021-01-11 21:07:12 +0100

    incl. bonus 1

--- js/index.js
+++ js/index.js
@@ -56,3 +56,22 @@ if ( hacker1.localeCompare(hacker2) ===  0) {
 }
 
 
+//Bonus 1
+
+let loreIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas semper augue ut molestie. Morbi sit amet felis nec magna varius vehicula. Sed imperdiet orci eu purus tincidunt luctus. Vivamus erat metus, rhoncus at ligula eget, vestibulum tincidunt velit. Cras vestibulum eleifend sodales. Curabitur porta, magna sed blandit tincidunt, libero neque semper massa, vitae sagittis dolor tellus in nisi. Morbi id venenatis dolor, ac aliquet diam. Integer non efficitur mauris, sollicitudin blandit tellus. Duis sed congue neque. Phasellus et molestie lorem, sit amet viverra diam. Fusce gravida sollicitudin laoreet. Sed cursus nunc est, at feugiat nisl suscipit sit amet. Ut vulputate magna non lobortis tincidunt. Sed molestie rhoncus commodo. Nulla vitae est quis metus posuere ornare vel rhoncus augue. Curabitur vel facilisis justo, in accumsan velit. Vestibulum ultricies, massa non lacinia mollis, ex arcu rutrum nulla, ac accumsan sapien nulla in diam. Nullam in nisl sollicitudin, efficitur quam ac, placerat arcu. Ut bibendum lobortis velit, eu accumsan turpis. Nulla mattis elit nulla, nec tincidunt massa suscipit dapibus. Suspendisse tincidunt magna justo, eget gravida purus gravida et. Aliquam dapibus lorem ut ipsum pretium convallis. Sed feugiat libero at justo rutrum pellentesque. In vitae est non diam porttitor eleifend vel nec lacus. Etiam et metus convallis, mollis quam sit amet, euismod diam. Integer non tortor nec magna fermentum tempor. Praesent in iaculis leo. Integer nec ultrices ex."
+
+let loreIpsumWords = loreIpsum.split(' ')
+console.log(loreIpsumWords)
+
+let loreIpsumWordsCount = loreIpsumWords.length
+console.log(loreIpsumWordsCount)
+
+let etCounter = 0;
+ 
+  for (let i = 0; i < loreIpsumWords.length; i++) {
+    if ( loreIpsumWords[i] === 'et' || loreIpsumWords[i] === 'Et') {
+       etCounter++
+    }
+  }
+
+ console.log(etCounter)
